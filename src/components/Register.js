44bb2b4c59044b5963d94bf575af5002fa53e0c1
Maria Olsen <69918976+mcolsen@users.noreply.github.com>
2020-10-19 14:34:07 -0600

    Basic CSS layout for register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -43,55 +43,57 @@ function Register() {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Name
-				<input
-					name="name"
-					type="text"
-					onChange={handleChange}
-					value={form.name}
-				/>
-			</label>
-			<label>
-				Business Name
-				<input
-					name="business"
-					type="text"
-					onChange={handleChange}
-					value={form.business}
-				/>
-			</label>
-			<label>
-				Email
-				<input
-					name="email"
-					type="text"
-					onChange={handleChange}
-					value={form.email}
-				/>
-			</label>
-			<label>
-				Password
-				<input
-					name="password"
-					type="password"
-					onChange={handleChange}
-					value={form.password}
-				/>
-			</label>
-			<label>
-				I have read and agree to the Terms of Service
-				<input
-					name="tos"
-					type="checkbox"
-					onChange={handleChange}
-					value={form.tos}
-					checked={form.tos}
-				/>
-			</label>
-			<button disabled={!buttonOn}>Submit</button>
-		</form>
+		<div className="register">
+			<form onSubmit={handleSubmit}>
+				<label>
+					Name
+					<input
+						name="name"
+						type="text"
+						onChange={handleChange}
+						value={form.name}
+					/>
+				</label>
+				<label>
+					Business Name
+					<input
+						name="business"
+						type="text"
+						onChange={handleChange}
+						value={form.business}
+					/>
+				</label>
+				<label>
+					Email
+					<input
+						name="email"
+						type="text"
+						onChange={handleChange}
+						value={form.email}
+					/>
+				</label>
+				<label>
+					Password
+					<input
+						name="password"
+						type="password"
+						onChange={handleChange}
+						value={form.password}
+					/>
+				</label>
+				<label>
+					I have read and agree to the Terms of Service
+					<input
+						name="tos"
+						type="checkbox"
+						onChange={handleChange}
+						value={form.tos}
+						checked={form.tos}
+					/>
+				</label>
+				<button disabled={!buttonOn}>Submit</button>
+			</form>
+		</div>
 	);
 }
 
